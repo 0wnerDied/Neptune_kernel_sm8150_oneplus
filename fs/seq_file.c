655ac556eba28676ca0046c57a35e6eb99914503
Andrei Vagin <avagin@openvz.org>
2018-04-10 16:31:23 -0700

    proc: optimize single-symbol delimiters to spead up seq_put_decimal_ull
    A delimiter is a string which is printed before a number.  A
    syngle-symbol delimiters can be printed by set_putc() and this works
    faster than printing by set_puts().
    
    == test_proc.c
    
    int main(int argc, char **argv)
    {
    	int n, i, fd;
    	char buf[16384];
    
    	n = atoi(argv[1]);
    	for (i = 0; i < n; i++) {
    		fd = open(argv[2], O_RDONLY);
    		if (fd < 0)
    			return 1;
    		if (read(fd, buf, sizeof(buf)) <= 0)
    			return 1;
    		close(fd);
    	}
    
    	return 0;
    }
    ==
    
    $ time ./test_proc  1000000 /proc/1/stat
    
    == Before patch ==
      real	0m3.820s
      user	0m0.337s
      sys	0m3.394s
    
    == After patch ==
      real	0m3.110s
      user	0m0.324s
      sys	0m2.700s
    
    Link: http://lkml.kernel.org/r/20180212074931.7227-3-avagin@openvz.org
    Signed-off-by: Andrei Vagin <avagin@openvz.org>
    Cc: Alexey Dobriyan <adobriyan@gmail.com>
    Cc: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- fs/seq_file.c
+++ fs/seq_file.c
@@ -706,9 +706,6 @@ void seq_put_decimal_ull_width(struct seq_file *m, const char *delimiter,
 	if (!width)
 		width = 1;
 
-	if (!width)
-		width = 1;
-
 	if (m->count + width >= m->size)
 		goto overflow;
 
