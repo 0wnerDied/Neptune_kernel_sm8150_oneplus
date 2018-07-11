18a08e0424b5b76514418440868099db64b7f318
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2018-07-11 14:36:49 -0700

    rcu: Eliminate synchronize_rcu_mult()
    Now that synchronize_rcu() waits for both RCU read-side critical
    sections and preempt-disabled regions of code, the sole caller of
    synchronize_rcu_mult() can be replaced by synchronize_rcu().
    This patch makes this change and removes synchronize_rcu_mult().
    Note that _wait_rcu_gp() still supports synchronize_rcu_mult(),
    and thus might be simplified in the future to take only take
    a single call_rcu() function rather than the current list of them.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: E-Neptune <z1281552865@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6346,10 +6346,6 @@ int sched_cpu_deactivate(unsigned int cpu)
 	 *
 	 * Do sync before park smpboot threads to take care the rcu boost case.
 	 */
-
-#ifdef CONFIG_PREEMPT
-	synchronize_sched();
-#endif
 	synchronize_rcu();
 
 #ifdef CONFIG_SCHED_SMT
