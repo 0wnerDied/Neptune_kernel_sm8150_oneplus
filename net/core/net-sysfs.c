248dcd13a5c26ad8ca9a6f9c74cb5465bbe02db1
Kimberly Brown <kimbrownkd@gmail.com>
2019-04-01 22:51:35 -0400

    net-sysfs: Replace ktype default_attrs field with groups
    The kobj_type default_attrs field is being replaced by the
    default_groups field. Replace the default_attrs fields in rx_queue_ktype
    and netdev_queue_ktype with default_groups. Use the ATTRIBUTE_GROUPS
    macro to create rx_queue_default_groups and netdev_queue_default_groups.
    
    This patch was tested by verifying that the sysfs files for the
    attributes in the default groups were created.
    
    Signed-off-by: Kimberly Brown <kimbrownkd@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: atndko <z1281552865@gmail.com>

--- net/core/net-sysfs.c
+++ net/core/net-sysfs.c
@@ -861,6 +861,7 @@ static struct attribute *rx_queue_default_attrs[] __ro_after_init = {
 #endif
 	NULL
 };
+ATTRIBUTE_GROUPS(rx_queue_default);
 
 static void rx_queue_release(struct kobject *kobj)
 {
@@ -901,7 +902,7 @@ static const void *rx_queue_namespace(struct kobject *kobj)
 static struct kobj_type rx_queue_ktype __ro_after_init = {
 	.sysfs_ops = &rx_queue_sysfs_ops,
 	.release = rx_queue_release,
-	.default_attrs = rx_queue_default_attrs,
+	.default_groups = rx_queue_default_groups,
 	.namespace = rx_queue_namespace
 };
 
@@ -1293,6 +1294,7 @@ static struct attribute *netdev_queue_default_attrs[] __ro_after_init = {
 #endif
 	NULL
 };
+ATTRIBUTE_GROUPS(netdev_queue_default);
 
 static void netdev_queue_release(struct kobject *kobj)
 {
@@ -1317,7 +1319,7 @@ static const void *netdev_queue_namespace(struct kobject *kobj)
 static struct kobj_type netdev_queue_ktype __ro_after_init = {
 	.sysfs_ops = &netdev_queue_sysfs_ops,
 	.release = netdev_queue_release,
-	.default_attrs = netdev_queue_default_attrs,
+	.default_groups = netdev_queue_default_groups,
 	.namespace = netdev_queue_namespace,
 };
 
