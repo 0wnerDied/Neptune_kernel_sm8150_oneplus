0046fb81136b93e67d71a63366604684407807aa
Park Ju Hyung <qkrwngud825@gmail.com>
2019-07-12 00:41:27 +0900

    sdcardfs: use kmem_cache pool for struct sdcardfs_file_info
    These get allocated and freed millions of times on this kernel tree.
    
    Use a dedicated kmem_cache pool and avoid costly dynamic memory allocations.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: atndko <z1281552865@gmail.com>

--- fs/sdcardfs/file.c
+++ fs/sdcardfs/file.c
@@ -23,6 +23,8 @@
 #include <linux/backing-dev.h>
 #endif
 
+struct kmem_cache *kmem_file_info_pool;
+
 static ssize_t sdcardfs_read(struct file *file, char __user *buf,
 			   size_t count, loff_t *ppos)
 {
@@ -259,7 +261,7 @@ static int sdcardfs_open(struct inode *inode, struct file *file)
 
 	file->f_mode |= FMODE_NONMAPPABLE;
 	file->private_data =
-		kzalloc(sizeof(struct sdcardfs_file_info), GFP_KERNEL);
+		kmem_cache_zalloc(kmem_file_info_pool, GFP_KERNEL);
 	if (!SDCARDFS_F(file)) {
 		err = -ENOMEM;
 		goto out_revert_cred;
@@ -290,7 +292,7 @@ static int sdcardfs_open(struct inode *inode, struct file *file)
 	}
 
 	if (err)
-		kfree(SDCARDFS_F(file));
+		kmem_cache_free(kmem_file_info_pool, SDCARDFS_F(file));
 	else
 		sdcardfs_copy_and_fix_attrs(inode, sdcardfs_lower_inode(inode));
 
@@ -326,7 +328,7 @@ static int sdcardfs_file_release(struct inode *inode, struct file *file)
 		fput(lower_file);
 	}
 
-	kfree(SDCARDFS_F(file));
+	kmem_cache_free(kmem_file_info_pool, SDCARDFS_F(file));
 	return 0;
 }
 
