aa860ab2bee5c673291b69694a6a12d214f1fd7b
Park Ju Hyung <qkrwngud825@gmail.com>
2019-10-08 23:41:33 +0900

    flasher: Load properties externally
    Load a separate "props" file for better modularity
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    
    This commit squashes following commits:
      https://github.com/arter97/AnyKernel3/commit/39b8346d388416f9ca9889b61a39924ff6feed91
      https://github.com/arter97/AnyKernel3/commit/248d8ed
    
    Signed-off-by: atndko <z1281552865@gmail.com>

--- flasher/anykernel.sh
+++ flasher/anykernel.sh
@@ -1,34 +1,16 @@
 # AnyKernel3 Ramdisk Mod Script
 # osm0sis @ xda-developers
 
-## AnyKernel setup
-# begin properties
-properties() { '
-kernel.string=Neptune Kernel by Ciel_U.T
-do.devicecheck=1
-do.modules=0
-do.systemless=1
-do.cleanup=1
-do.cleanuponabort=0
-device.name1=OnePlus7
-device.name2=guacamoleb
-device.name3=OnePlus7Pro
-device.name4=guacamole
-device.name5=OnePlus7ProTMO
-device.name6=guacamolet
-device.name7=OnePlus7T
-device.name8=hotdogb
-device.name9=OnePlus7TPro
-device.name10=hotdog
-device.name11=OnePlus7TProNR
-device.name12=hotdogg
-supported.versions=10
-'; } # end properties
+# set up working directory variables
+test "$home" || home=$PWD;
+bootimg=$home/boot.img;
+bin=$home/tools;
+patch=$home/patch;
+ramdisk=$home/ramdisk;
+split_img=$home/split_img;
 
-# shell variables
-block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=1;
-ramdisk_compression=auto;
+## AnyKernel setup
+eval $(cat $home/props | grep -v '\.')
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
