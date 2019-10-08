675c52524daf60642dbf9a9d73ddc97b56abe356
Park Ju Hyung <qkrwngud825@gmail.com>
2019-10-08 23:41:33 +0900

    Load properties externally
    Load a separate "props" file for better modularity
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: atndko <z1281552865@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -2,28 +2,7 @@
 # osm0sis @ xda-developers
 
 ## AnyKernel setup
-# begin properties
-properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
-do.devicecheck=1
-do.modules=0
-do.systemless=1
-do.cleanup=1
-do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
-supported.versions=
-supported.patchlevels=
-'; } # end properties
-
-# shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
-is_slot_device=0;
-ramdisk_compression=auto;
-
+eval $(cat /tmp/anykernel/props | grep -v '\.')
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
