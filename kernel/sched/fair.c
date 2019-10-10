ad7cb65faafd1a29e9ed945967ce1f1c0831c297
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-10-10 11:49:59 +0530

    sched: fair: Do not boost cpu/task utilization if boost = 2
    For the same reasons as a6d616a6ab860156d98d37563a2bb2ece0a116be, but still allowing a user
    configuration if a user does want the extra performance the corresponding
    power costs.
    
    Choose a boost value of 2 for this as it's not typically set as much as
    something like 1, 5, 10, etc. would be used.
    
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6771,7 +6771,7 @@ schedtune_cpu_margin(unsigned long util, int cpu)
 {
 	int boost = schedtune_cpu_boost(cpu);
 
-	if (boost == 0)
+	if (boost == 0 || boost == 2)
 		return 0;
 
 	return schedtune_margin(util, boost);
@@ -6784,7 +6784,7 @@ schedtune_task_margin(struct task_struct *task)
 	unsigned long util;
 	long margin;
 
-	if (boost == 0)
+	if (boost == 0 || boost == 2)
 		return 0;
 
 	util = task_util_est(task);
