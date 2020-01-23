timocapa/kernel_oneplus_msm8998

cpu_input_boost: Allow modifying max boost duration in userspace for certain events

drivers/cpufreq/cpu_input_boost.c
@@ -34,6 +34,9 @@ static unsigned short input_boost_duration __read_mostly =
 static unsigned short wake_boost_duration __read_mostly =
 	CONFIG_WAKE_BOOST_DURATION_MS;
 
+unsigned short cib_max_boost_duration __read_mostly =
+	CONFIG_MAX_BOOST_DURATION_MS;
+
 module_param(input_boost_freq_lp, uint, 0644);
 module_param(input_boost_freq_hp, uint, 0644);
 module_param_named(remove_input_boost_freq_lp,
@@ -46,6 +49,7 @@ module_param(max_boost_freq_hp, uint, 0644);
 module_param(powerhal_boost_duration, short, 0644);
 module_param(input_boost_duration, short, 0644);
 module_param(wake_boost_duration, short, 0644);
+module_param(cib_max_boost_duration, short, 0644);
 
 enum {
 	SCREEN_OFF,

include/linux/cpu_input_boost.h
@@ -8,6 +8,7 @@
 #ifdef CONFIG_CPU_INPUT_BOOST
 void cpu_input_boost_kick(void);
 void cpu_input_boost_kick_max(unsigned int duration_ms);
+extern unsigned short cib_max_boost_duration;
 #else
 static inline void cpu_input_boost_kick(void)
 {

0wnerDied/Neptune_kernel_sm8150_oneplus

drivers/cpufreq/cpu_input_boost.c
@@ -44,6 +44,8 @@ static unsigned short input_boost_duration __read_mostly =
 	CONFIG_INPUT_BOOST_DURATION_MS;
 static unsigned short wake_boost_duration __read_mostly =
 	CONFIG_WAKE_BOOST_DURATION_MS;
+unsigned short app_launch_boost_duration __read_mostly =
+	CONFIG_MAX_BOOST_DURATION_MS;
 
 module_param(input_boost_freq_little, uint, 0644);
 module_param(input_boost_freq_big, uint, 0644);
@@ -58,6 +60,7 @@ module_param(cpu_freq_idle_little, uint, 0644);
 
 module_param(input_boost_duration, short, 0644);
 module_param(wake_boost_duration, short, 0644);
+module_param(app_launch_boost_duration, short, 0644);
 
 enum {
 	SCREEN_ON,

include/linux/cpu_input_boost.h
@@ -8,6 +8,7 @@
 #ifdef CONFIG_CPU_INPUT_BOOST
 void cpu_input_boost_kick(void);
 void cpu_input_boost_kick_max(unsigned int duration_ms);
+extern unsigned short app_launch_boost_duration;
 #else
 static inline void cpu_input_boost_kick(void)
 {