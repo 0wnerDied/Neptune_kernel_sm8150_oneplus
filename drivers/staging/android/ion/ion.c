96e15d9c4382bcb0e80fab097666fbb4d8d4e631
Park Ju Hyung <qkrwngud825@gmail.com>
2020-02-16 00:51:35 +0900

    staging: android: ion: move redundant table variable
    Move those to under CONFIG_ION_FORCE_DMA_SYNC.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -110,7 +110,6 @@ static struct ion_buffer *ion_buffer_create(struct ion_heap *heap,
 					    unsigned long flags)
 {
 	struct ion_buffer *buffer;
-	struct sg_table *table;
 	int ret;
 
 	buffer = kzalloc(sizeof(*buffer), GFP_KERNEL);
@@ -142,7 +141,6 @@ static struct ion_buffer *ion_buffer_create(struct ion_heap *heap,
 		ret = -EINVAL;
 		goto err1;
 	}
-	table = buffer->sg_table;
 
 	INIT_LIST_HEAD(&buffer->attachments);
 	INIT_LIST_HEAD(&buffer->vmas);
@@ -151,6 +149,7 @@ static struct ion_buffer *ion_buffer_create(struct ion_heap *heap,
 	if (IS_ENABLED(CONFIG_ION_FORCE_DMA_SYNC)) {
 		int i;
 		struct scatterlist *sg;
+		struct sg_table *table;
 
 		/*
 		 * this will set up dma addresses for the sglist -- it is not
@@ -159,6 +158,7 @@ static struct ion_buffer *ion_buffer_create(struct ion_heap *heap,
 		 * practice on our systems the only dma_address space is
 		 * physical addresses.
 		 */
+		table = buffer->sg_table;
 		for_each_sg(table->sgl, sg, table->nents, i) {
 			sg_dma_address(sg) = sg_phys(sg);
 			sg_dma_len(sg) = sg->length;
