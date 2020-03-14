7330a8a3b02605312c6339cfa0cd17a6988c0baf
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2020-03-15 04:40:46 +0530

    qcacld-3.0: Nullify some debugging

--- drivers/staging/qcacld-3.0/core/sap/src/sap_internal.h
+++ drivers/staging/qcacld-3.0/core/sap/src/sap_internal.h
@@ -72,7 +72,7 @@ extern "C" {
 #define sap_err(params...) QDF_TRACE_ERROR(QDF_MODULE_ID_SAP, params)
 #define sap_warn(params...) QDF_TRACE_WARN(QDF_MODULE_ID_SAP, params)
 #define sap_info(params...) QDF_TRACE_INFO(QDF_MODULE_ID_SAP, params)
-#define sap_debug(params...) QDF_TRACE_DEBUG(QDF_MODULE_ID_SAP, params)
+#define sap_debug(params...)
 
 #define sap_nofl_alert(params...) \
 	QDF_TRACE_FATAL_NO_FL(QDF_MODULE_ID_SAP, params)
@@ -82,14 +82,13 @@ extern "C" {
 	QDF_TRACE_WARN_NO_FL(QDF_MODULE_ID_SAP, params)
 #define sap_nofl_info(params...) \
 	QDF_TRACE_INFO_NO_FL(QDF_MODULE_ID_SAP, params)
-#define sap_nofl_debug(params...) \
-	QDF_TRACE_DEBUG_NO_FL(QDF_MODULE_ID_SAP, params)
+#define sap_nofl_debug(params...)
 
 #define sap_alert_rl(params...) QDF_TRACE_FATAL_RL(QDF_MODULE_ID_SAP, params)
 #define sap_err_rl(params...) QDF_TRACE_ERROR_RL(QDF_MODULE_ID_SAP, params)
 #define sap_warn_rl(params...) QDF_TRACE_WARN_RL(QDF_MODULE_ID_SAP, params)
 #define sap_info_rl(params...) QDF_TRACE_INFO_RL(QDF_MODULE_ID_SAP, params)
-#define sap_debug_rl(params...) QDF_TRACE_DEBUG_RL(QDF_MODULE_ID_SAP, params)
+#define sap_debug_rl(params...)
 
 /*----------------------------------------------------------------------------
  *  Typedefs
