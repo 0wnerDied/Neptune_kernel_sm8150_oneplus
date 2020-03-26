dc3400784f77ce4a7a3c4de78762d0ccb6513ee6
Akhil P Oommen <akhilpo@codeaurora.org>
2020-03-26 19:36:41 +0530

    msm: kgsl: Fix skipping aperture programming
    During the first adreno_start(), we initialize the global PT and
    make an scm call to program the aperture related registers. This
    programing is skipped when scm_is_call_available() call fails.
    Skipping aperture programing can make CP get stuck during a
    context switch. To fix this, avoid scm_is_call_available() call
    and force aperture programing from secure world for all a6xx GPUs.
    
    Change-Id: I13923afbece0be12b1a9336b7d20c3039164a6b6
    Signed-off-by: Akhil P Oommen <akhilpo@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/gpu/msm/kgsl_mmu.h
+++ drivers/gpu/msm/kgsl_mmu.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2002,2007-2019, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2002,2007-2020, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -142,6 +142,8 @@ struct kgsl_mmu_pt_ops {
 #define KGSL_MMU_IO_COHERENT BIT(10)
 /* The device requires VA mappings padded up to a given size */
 #define KGSL_MMU_PAD_VA BIT(11)
+/* The device supports aperture programming from secure world */
+#define KGSL_MMU_SMMU_APERTURE BIT(12)
 
 /**
  * struct kgsl_mmu - Master definition for KGSL MMU devices
