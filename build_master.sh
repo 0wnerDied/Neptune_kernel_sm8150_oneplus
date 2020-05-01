800c6dfb19814b1e665377526f3849e5fc156962
E-Neptune <z1281552865@gmail.com>
2020-05-01 16:07:43 +0800

    version R1

--- build_master.sh
+++ build_master.sh
@@ -12,16 +12,16 @@ if [ -e arch/arm64/boot/Image.gz ] ; then
 	echo
 	echo "Building Kernel Package"
 	echo
-	rm NeptuneKernel-$VERSION.zip 2>/dev/null
+	rm HentaiKernel-$VERSION.zip 2>/dev/null
 	rm -rf kernelzip 2>/dev/null
 	mkdir kernelzip
 	cp -rp /home/zhulitao/anykernel/* kernelzip/
 	find arch/arm64/boot/dts -name '*.dtb' -exec cat {} + > kernelzip/dtb
 	cd kernelzip/
-	7z a -mx9 NeptuneKernel-$VERSION-tmp.zip *
-	7z a -mx0 NeptuneKernel-$VERSION-tmp.zip ../arch/arm64/boot/Image.gz
-	zipalign -v 4 NeptuneKernel-$VERSION-tmp.zip ../NeptuneKernel-$VERSION.zip
-	rm NeptuneKernel-$VERSION-tmp.zip
+	7z a -mx9 HentaiKernel-$VERSION-tmp.zip *
+	7z a -mx0 HentaiKernel-$VERSION-tmp.zip ../arch/arm64/boot/Image.gz
+	zipalign -v 4 HentaiKernel-$VERSION-tmp.zip ../HentaiKernel-$VERSION.zip
+	rm HentaiKernel-$VERSION-tmp.zip
 	cd ..
-	ls -al NeptuneKernel-$VERSION.zip
+	ls -al HentaiKernel-$VERSION.zip
 fi
