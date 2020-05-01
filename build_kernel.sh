e414061c6c744086a7987113d85258332ff31fb2
E-Neptune <z1281552865@gmail.com>
2020-05-01 14:38:02 +0800

    update my settings

---
+++ build_kernel.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export ARCH=arm64
+export SUBARCH=arm64
+export CLANG_PATH=/home/zhulitao/clang/bin
+export PATH=${CLANG_PATH}:${PATH}
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export CROSS_COMPILE=/home/zhulitao/clang/bin/aarch64-linux-gnu-
+export CROSS_COMPILE_ARM32=/home/zhulitao/clang/bin/arm-linux-gnueabi-
+export LD_LIBRARY_PATH=/home/zhulitao/clang/lib:$LD_LIBRARY_PATH
+
+echo
+echo "Setting defconfig"
+echo
+# cp defconfig .config
+make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip weeb_defconfig
+
+echo
+echo "Compiling kernel"
+echo 
+
+make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip -j$(nproc --all) || exit 1
---
+++ build_master.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+VERSION="$(cat version)"
+
+if [[ "${1}" != "skip" ]] ; then
+	./build_clean.sh
+fi
+
+./build_kernel.sh || exit 1
+
+if [ -e arch/arm64/boot/Image.gz ] ; then
+	echo
+	echo "Building Kernel Package"
+	echo
+	rm NeptuneKernel-$VERSION.zip 2>/dev/null
+	rm -rf kernelzip 2>/dev/null
+	mkdir kernelzip
+	cp -rp ./anykernel/* kernelzip/
+	find arch/arm64/boot/dts -name '*.dtb' -exec cat {} + > kernelzip/dtb
+	cd kernelzip/
+	7z a -mx9 NeptuneKernel-$VERSION-tmp.zip *
+	7z a -mx0 NeptuneKernel-$VERSION-tmp.zip ../arch/arm64/boot/Image.gz
+	zipalign -v 4 NeptuneKernel-$VERSION-tmp.zip ../NeptuneKernel-$VERSION.zip
+	rm NeptuneKernel-$VERSION-tmp.zip
+	cd ..
+	ls -al NeptuneKernel-$VERSION.zip
+fi
