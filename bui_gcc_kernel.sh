c63c496ace9624d143c2b17caa02d803ab58919d
E-Neptune <z1281552865@gmail.com>
2020-05-19 21:44:39 +0800

    update bui.sh

--- bui_gcc_kernel.sh
+++ bui_gcc_kernel.sh
@@ -4,6 +4,7 @@ export ARCH=arm64
 export SUBARCH=arm64
 export CROSS_COMPILE=/home/zhulitao/aarch64-linux-elf/bin/aarch64-linux-elf-
 export CROSS_COMPILE_ARM32=/home/zhulitao/arm-linux-eabi/bin/arm-linux-eabi-
+export KBUILD_BUILD_HOST=Neptune
 
 echo
 echo "Setting defconfig"
--- build_kernel.sh
+++ build_kernel.sh
@@ -8,6 +8,7 @@ export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=/home/zhulitao/clang/bin/aarch64-linux-gnu-
 export CROSS_COMPILE_ARM32=/home/zhulitao/clang/bin/arm-linux-gnueabi-
 export LD_LIBRARY_PATH=/home/zhulitao/clang/lib:$LD_LIBRARY_PATH
+export KBUILD_BUILD_HOST=Neptune
 
 echo
 echo "Setting defconfig"
