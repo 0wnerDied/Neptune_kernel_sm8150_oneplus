beb43f32c4019a86223d51e88eed1efd09f4a17b
E-Neptune <z1281552865@gmail.com>
2020-05-21 21:31:35 +0800

    Revert "f2fs: remove blk_plugging in block_operations"
    This reverts commit 41e6f49c2f39628a27f7600527c254f011c9a9fa.

--- fs/f2fs/checkpoint.c
+++ fs/f2fs/checkpoint.c
@@ -1163,7 +1163,10 @@ static int block_operations(struct f2fs_sb_info *sbi)
 		.nr_to_write = LONG_MAX,
 		.for_reclaim = 0,
 	};
+	struct blk_plug plug;
 	int err = 0, cnt = 0;
+
+	blk_start_plug(&plug);
 
 retry_flush_quotas:
 	f2fs_lock_all(sbi);
@@ -1192,7 +1195,7 @@ retry_flush_dents:
 		f2fs_unlock_all(sbi);
 		err = f2fs_sync_dirty_inodes(sbi, DIR_INODE);
 		if (err)
-			return err;
+			goto out;
 		cond_resched();
 		goto retry_flush_quotas;
 	}
@@ -1208,7 +1211,7 @@ retry_flush_dents:
 		f2fs_unlock_all(sbi);
 		err = f2fs_sync_inode_meta(sbi);
 		if (err)
-			return err;
+			goto out;
 		cond_resched();
 		goto retry_flush_quotas;
 	}
@@ -1224,7 +1227,7 @@ retry_flush_nodes:
 		if (err) {
 			up_write(&sbi->node_change);
 			f2fs_unlock_all(sbi);
-			return err;
+			goto out;
 		}
 		cond_resched();
 		goto retry_flush_nodes;
@@ -1236,6 +1239,8 @@ retry_flush_nodes:
 	 */
 	__prepare_cp_block(sbi);
 	up_write(&sbi->node_change);
+out:
+	blk_finish_plug(&plug);
 	return err;
 }
 
