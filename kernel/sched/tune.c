5e394301a487c4899c9dc8e44f66d5e0e9bc31a4
Adam W. Willis <return.of.octobot@gmail.com>
2020-05-21 22:13:13 -0400

    sched/tune: Streamline SchedTune Assist logging
    Adapted from F1xy-kernels/RAPHAEL_Kernel@9f649c1
    
    [@0ctobot: Additionally, disable sched_boost_no_override]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -733,15 +733,14 @@ static void write_default_values(struct cgroup_subsys_state *css)
 		if (!strcmp(css->cgroup->kn->name, tgt.name)) {
 			boost_write(css, NULL, tgt.boost);
 			prefer_idle_write(css, NULL, tgt.prefer_idle);
-			prefer_high_cap_write(css, NULL, tgt.prefer_high_cap);
 			sched_boost_override_write(css, NULL, tgt.no_override);
 #ifndef CONFIG_SCHED_WALT
-			pr_info("stune_assist: setting values for %s: boost=%d prefer_idle=%d prefer_high_cap=%d no_override=%d\n",
-				tgt.name, tgt.boost, tgt.prefer_idle, tgt.prefer_high_cap, tgt.no_override);
+			pr_info("stune_assist: setting values for %s: boost=%d prefer_idle=%d no_override=%d\n",
+				tgt.name, tgt.boost, tgt.prefer_idle, tgt.no_override);
 #else
 			sched_colocate_write(css, NULL, tgt.colocate);
-			pr_info("stune_assist: setting values for %s: boost=%d prefer_idle=%d prefer_high_cap=%d colocate=%d no_override=%d\n",
-				tgt.name, tgt.boost, tgt.prefer_idle, tgt.prefer_high_cap, tgt.colocate, tgt.no_override);
+			pr_info("stune_assist: setting values for %s: boost=%d prefer_idle=%d colocate=%d no_override=%d\n",
+				tgt.name, tgt.boost, tgt.prefer_idle, tgt.colocate, tgt.no_override);
 #endif
 		}
 	}
