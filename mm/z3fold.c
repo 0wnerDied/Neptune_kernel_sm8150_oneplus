bc3a17eaf0a17f8b5558aff53f6885746e0cbd8b
Qian Cai <cai@lca.pw>
2020-05-27 22:20:40 -0700

    mm/z3fold: silence kmemleak false positives of slots
    Kmemleak reported many leaks while under memory pressue in,
    
        slots = alloc_slots(pool, gfp);
    
    which is referenced by "zhdr" in init_z3fold_page(),
    
        zhdr->slots = slots;
    
    However, "zhdr" could be gone without freeing slots as the later will be
    freed separately when the last "handle" off of "handles" array is freed.
    It will be within "slots" which is always aligned.
    
      unreferenced object 0xc000000fdadc1040 (size 104):
      comm "oom04", pid 140476, jiffies 4295359280 (age 3454.970s)
      hex dump (first 32 bytes):
        00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  ................
        00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  ................
      backtrace:
        z3fold_zpool_malloc+0x7b0/0xe10
        alloc_slots at mm/z3fold.c:214
        (inlined by) init_z3fold_page at mm/z3fold.c:412
        (inlined by) z3fold_alloc at mm/z3fold.c:1161
        (inlined by) z3fold_zpool_malloc at mm/z3fold.c:1735
        zpool_malloc+0x34/0x50
        zswap_frontswap_store+0x60c/0xda0
        zswap_frontswap_store at mm/zswap.c:1093
        __frontswap_store+0x128/0x330
        swap_writepage+0x58/0x110
        pageout+0x16c/0xa40
        shrink_page_list+0x1ac8/0x25c0
        shrink_inactive_list+0x270/0x730
        shrink_lruvec+0x444/0xf30
        shrink_node+0x2a4/0x9c0
        do_try_to_free_pages+0x158/0x640
        try_to_free_pages+0x1bc/0x5f0
        __alloc_pages_slowpath.constprop.60+0x4dc/0x15a0
        __alloc_pages_nodemask+0x520/0x650
        alloc_pages_vma+0xc0/0x420
        handle_mm_fault+0x1174/0x1bf0
    
    Signed-off-by: Qian Cai <cai@lca.pw>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Acked-by: Vitaly Wool <vitaly.wool@konsulko.com>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Link: http://lkml.kernel.org/r/20200522220052.2225-1-cai@lca.pw
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: atndko <z1281552865@gmail.com>

--- mm/z3fold.c
+++ mm/z3fold.c
@@ -41,6 +41,7 @@
 #include <linux/slab.h>
 #include <linux/spinlock.h>
 #include <linux/zpool.h>
+#include <linux/kmemleak.h>
 
 /*
  * NCHUNKS_ORDER determines the internal allocation granularity, effectively
@@ -213,6 +214,8 @@ static inline struct z3fold_buddy_slots *alloc_slots(struct z3fold_pool *pool,
 				 (gfp & ~(__GFP_HIGHMEM | __GFP_MOVABLE)));
 
 	if (slots) {
+		/* It will be freed separately in free_handle(). */
+		kmemleak_not_leak(slots);
 		memset(slots->slot, 0, sizeof(slots->slot));
 		slots->pool = (unsigned long)pool;
 		rwlock_init(&slots->lock);
