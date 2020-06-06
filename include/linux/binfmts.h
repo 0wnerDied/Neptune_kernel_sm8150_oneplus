3aecd23923fe5e770ff2c634fd15c986636cd476
E-Neptune <z1281552865@gmail.com>
2020-06-06 14:29:04 +0800

    binfmts: Don't block init.qcom.post_boot
    We are going to use execprog from arter97.
    Make some tweaks and replace /vender/bin/init.qcom.post_boot.
    So there's no need o block init.qcom.post_boot any more.
    
    Signed-off-by: E-Neptune <z1281552865@gmail.com>

--- include/linux/binfmts.h
+++ include/linux/binfmts.h
@@ -160,8 +160,7 @@ static inline bool task_is_booster(struct task_struct *tsk)
 	       !strcmp(comm, "perf@1.0-servic") ||
 	       !strcmp(comm, "perf@2.0-servic") ||
 	       !strcmp(comm, "power@2.0-servic") ||
-	       !strcmp(comm, "iop@") ||
-	       !strcmp(comm, "init.qcom.post_");
+	       !strcmp(comm, "iop@");
 }
 
 #endif /* _LINUX_BINFMTS_H */
