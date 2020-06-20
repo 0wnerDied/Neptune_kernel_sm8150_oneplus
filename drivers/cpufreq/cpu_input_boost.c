8d3e3145279543fa986a3d7fcd8dbf3f227b9a46
E-Neptune <z1281552865@gmail.com>
2020-06-20 11:52:36 +0800

    cpufreq: cpu_input_boost: Remove all idle cpu sets code
    Signed-off-by: E-Neptune <z1281552865@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -38,8 +38,6 @@ static unsigned int cpu_freq_min_big __read_mostly =
 	CONFIG_CPU_FREQ_MIN_PERF;
 static unsigned int cpu_freq_min_prime __read_mostly =
 	CONFIG_CPU_FREQ_MIN_PERFP;
-static unsigned int cpu_freq_idle_little __read_mostly =
-	CONFIG_CPU_FREQ_IDLE_LP;
 
 static unsigned short wake_boost_duration __read_mostly =
 	CONFIG_WAKE_BOOST_DURATION_MS;
@@ -53,7 +51,6 @@ module_param(max_boost_freq_prime, uint, 0644);
 module_param(cpu_freq_min_little, uint, 0644);
 module_param(cpu_freq_min_big, uint, 0644);
 module_param(cpu_freq_min_prime, uint, 0644);
-module_param(cpu_freq_idle_little, uint, 0644);
 
 module_param(wake_boost_duration, short, 0644);
 
@@ -123,21 +120,6 @@ static unsigned int get_min_freq(struct cpufreq_policy *policy)
 
 	return max(freq, policy->cpuinfo.min_freq);
 }
-
-static unsigned int get_idle_freq(struct cpufreq_policy *policy)
-{
-	unsigned int freq;
-
-	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
-		freq = cpu_freq_idle_little;
-	else if (cpumask_test_cpu(policy->cpu, cpu_perf_mask))
-		freq = CONFIG_CPU_FREQ_IDLE_PERF;
-	else
-		freq = CONFIG_CPU_FREQ_IDLE_PERFP;
-
-	return max(freq, policy->cpuinfo.min_freq);
-}
-
 
 static void update_online_cpu_policy(void)
 {
@@ -261,7 +243,7 @@ static int cpu_notifier_cb(struct notifier_block *nb, unsigned long action,
 
 	/* Unboost when the screen is off */
 	if (!test_bit(SCREEN_ON, &b->state)) {
-		policy->min = get_idle_freq(policy);
+		policy->min = get_min_freq(policy);
 		disable_schedtune_boost("top-app", true);
 		return NOTIFY_OK;
 	}
