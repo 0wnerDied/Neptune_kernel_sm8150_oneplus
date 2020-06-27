42af30b59e3cb31f9874cd0387e40c17502ca607
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2020-06-27 19:17:01 +0530

    Revert "sched: Improve the scheduler"
    This reverts commit aa7717df65699eda4fd245908013fab2bf8d250c.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5056,10 +5056,9 @@ unsigned int sched_lib_mask_force;
 static inline bool is_sched_lib_based_app(pid_t pid)
 {
 	const char *name = NULL;
-	char *libname, *lib_list;
+	char *lib_list, *libname;
 	struct vm_area_struct *vma;
 	char path_buf[LIB_PATH_LENGTH];
-	char tmp_lib_name[LIB_PATH_LENGTH];
 	bool found = false;
 	struct task_struct *p;
 	struct mm_struct *mm;
@@ -5067,11 +5066,14 @@ static inline bool is_sched_lib_based_app(pid_t pid)
 	if (strnlen(sched_lib_name, LIB_PATH_LENGTH) == 0)
 		return false;
 
+	lib_list = kstrdup(sched_lib_name, GFP_KERNEL);
+
 	rcu_read_lock();
 
 	p = find_process_by_pid(pid);
 	if (!p) {
 		rcu_read_unlock();
+		kfree(lib_list);
 		return false;
 	}
 
@@ -5091,14 +5093,11 @@ static inline bool is_sched_lib_based_app(pid_t pid)
 			if (IS_ERR(name))
 				goto release_sem;
 
-			strlcpy(tmp_lib_name, sched_lib_name, LIB_PATH_LENGTH);
-			lib_list = tmp_lib_name;
 			while ((libname = strsep(&lib_list, ","))) {
-				libname = skip_spaces(libname);
 				if (strnstr(name, libname,
 					strnlen(name, LIB_PATH_LENGTH))) {
 					found = true;
-					goto release_sem;
+					break;
 				}
 			}
 		}
@@ -5109,6 +5108,7 @@ release_sem:
 	mmput(mm);
 put_task_struct:
 	put_task_struct(p);
+	kfree(lib_list);
 	return found;
 }
 
