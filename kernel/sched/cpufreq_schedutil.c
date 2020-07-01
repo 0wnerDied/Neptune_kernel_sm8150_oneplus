febcfbddacc76d8eae7e4388dd5b9e7d75361c21
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2020-07-02 00:14:38 +0530

    Revert "cpufreq: schedutil: Ignore work_in_progress"
    Revert for upcoming changes from mainline and CAF 4.19.
    This reverts commit 73f29d2e6c4fb080efa1c866e347e43d5bc76233.
    
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -105,6 +105,9 @@ static bool sugov_should_update_freq(struct sugov_policy *sg_policy, u64 time)
 	if (!cpufreq_this_cpu_can_update(sg_policy->policy))
 		return false;
 
+	if (sg_policy->work_in_progress)
+		return false;
+
 	if (unlikely(sg_policy->need_freq_update)) {
 		sg_policy->need_freq_update = false;
 		/*
