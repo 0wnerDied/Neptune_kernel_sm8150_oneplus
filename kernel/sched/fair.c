e2392286f8098e7134f34ad5b627e5f125e63891
Wei Wang <wvw@google.com>
2020-07-15 22:57:41 -0700

    sched: fair: placement optimization for heavy load
    Previously we have used pure CFS wakeup in overutilized case. This is a
    tweaked version to activate the path only for important tasks.
    
    Bug: 161190988
    Bug: 160883639
    Test: boot and systrace
    Signed-off-by: Wei Wang <wvw@google.com>
    Change-Id: I2a27f241b3ba32a04cf6f88deb483d6636440dcf
    Signed-off-by: atndko <z1281552865@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7809,6 +7809,13 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 					continue;
 
 				/*
+				 * Skip searching for active CPU for tasks have
+				 * high priority & boosted.
+				 */
+				if (boosted && p->prio <= DEFAULT_PRIO)
+					continue;
+
+				/*
 				 * Case A.2: Target ACTIVE CPU
 				 * Favor CPUs with max spare capacity.
 				 */
@@ -8038,8 +8045,10 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 		: best_idle_cpu;
 
 	if (target_cpu == -1 && most_spare_cap_cpu != -1 &&
-		/* ensure we use active cpu for active migration */
-		!(p->state == TASK_RUNNING && !idle_cpu(most_spare_cap_cpu)))
+	    /* ensure we use active cpu for active migration */
+	    !(p->state == TASK_RUNNING && !idle_cpu(most_spare_cap_cpu)) &&
+		/* do not pick an overutilized most_spare_cap_cpu */
+		!cpu_overutilized(most_spare_cap_cpu))
 		target_cpu = most_spare_cap_cpu;
 
 	trace_sched_find_best_target(p, prefer_idle, min_util, cpu,
