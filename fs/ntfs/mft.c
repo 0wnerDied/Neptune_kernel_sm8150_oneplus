007c29e0925dea7d93fdac79013ba346acb8a9dd
E-Neptune <z1281552865@gmail.com>
2020-07-27 15:52:04 +0800

    Revert "[PATCH] ntfs: convert to new i_version API"
    This reverts commit 9ceaf9a3472c2a4eb3a95f28807c15243d65d813.

--- fs/ntfs/mft.c
+++ fs/ntfs/mft.c
@@ -24,7 +24,6 @@
 #include <linux/slab.h>
 #include <linux/swap.h>
 #include <linux/bio.h>
-#include <linux/iversion.h>
 
 #include "attrib.h"
 #include "aops.h"
@@ -2655,7 +2654,7 @@ mft_rec_already_initialized:
 		 * file so that the file can be updated if necessary (compare
 		 * with f_version).
 		 */
-		inode_set_iversion(vi, 1);
+		vi->i_version = 1;
 
 		/* The owner and group come from the ntfs volume. */
 		vi->i_uid = vol->uid;
