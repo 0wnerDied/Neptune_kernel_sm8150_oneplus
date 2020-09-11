fad9e9f75987304b84dc779b6e151f9ab3691907
E-Neptune <z1281552865@gmail.com>
2020-09-11 20:48:34 +0800

    Revert "scsi: ufs: adjust WAIT_AFTER_REF_CLK_UNGATE sleep times"
    This reverts commit d1f60a1058650b0fdcdeeed1479d0213ada82f8e.
    
    Signed-off-by: atndko <z1281552865@gmail.com>

--- drivers/scsi/ufs/ufs-qcom.c
+++ drivers/scsi/ufs/ufs-qcom.c
@@ -1388,9 +1388,9 @@ static void ufs_qcom_dev_ref_clk_ctrl(struct ufs_qcom_host *host, bool enable)
 		if (enable) {
 			if (host->hba->dev_info.quirks &
 			    UFS_DEVICE_QUIRK_WAIT_AFTER_REF_CLK_UNGATE)
-				usleep_range(960, 970);
+				usleep_range(50, 60);
 			else
-				usleep_range(200, 210);
+				udelay(1);
 		}
 
 		host->is_dev_ref_clk_enabled = enable;
