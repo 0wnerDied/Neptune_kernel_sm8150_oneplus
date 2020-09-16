172da93d54f00acfd7cea80133ef7d8070d9e466
atndko <z1281552865@gmail.com>
2020-09-16 22:41:33 +0800

    Revert "btrfs: sysfs: Replace default_attrs in ktypes with groups"
    This reverts commit 978d35c84a0623cbe1912cb9b49a233b3047b2ba.
    
    Signed-off-by: atndko <z1281552865@gmail.com>

--- fs/btrfs/sysfs.c
+++ fs/btrfs/sysfs.c
@@ -287,12 +287,11 @@ static ssize_t raid_bytes_show(struct kobject *kobj,
 	return snprintf(buf, PAGE_SIZE, "%llu\n", val);
 }
 
-static struct attribute *raid_attrs[] = {
+static struct attribute *raid_attributes[] = {
 	BTRFS_RAID_ATTR_PTR(total_bytes),
 	BTRFS_RAID_ATTR_PTR(used_bytes),
 	NULL
 };
-ATTRIBUTE_GROUPS(raid);
 
 static void release_raid_kobj(struct kobject *kobj)
 {
@@ -302,7 +301,7 @@ static void release_raid_kobj(struct kobject *kobj)
 struct kobj_type btrfs_raid_ktype = {
 	.sysfs_ops = &kobj_sysfs_ops,
 	.release = release_raid_kobj,
-	.default_groups = raid_groups,
+	.default_attrs = raid_attributes,
 };
 
 #define SPACE_INFO_ATTR(field)						\
@@ -348,7 +347,6 @@ static struct attribute *space_info_attrs[] = {
 	BTRFS_ATTR_PTR(total_bytes_pinned),
 	NULL,
 };
-ATTRIBUTE_GROUPS(space_info);
 
 static void space_info_release(struct kobject *kobj)
 {
@@ -360,7 +358,7 @@ static void space_info_release(struct kobject *kobj)
 struct kobj_type space_info_ktype = {
 	.sysfs_ops = &kobj_sysfs_ops,
 	.release = space_info_release,
-	.default_groups = space_info_groups,
+	.default_attrs = space_info_attrs,
 };
 
 static const struct attribute *allocation_attrs[] = {
