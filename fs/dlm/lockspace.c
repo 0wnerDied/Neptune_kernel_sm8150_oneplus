0ba0d03da012ba227b998461a6a094c00935a898
atndko <z1281552865@gmail.com>
2020-09-16 22:41:48 +0800

    Revert "dlm: Replace default_attrs in dlm_ktype with default_groups"
    This reverts commit f43524956121b21552827799d83b3f4798185dff.
    
    Signed-off-by: atndko <z1281552865@gmail.com>

--- fs/dlm/lockspace.c
+++ fs/dlm/lockspace.c
@@ -160,7 +160,6 @@ static struct attribute *dlm_attrs[] = {
 	&dlm_attr_recover_nodeid.attr,
 	NULL,
 };
-ATTRIBUTE_GROUPS(dlm);
 
 static ssize_t dlm_attr_show(struct kobject *kobj, struct attribute *attr,
 			     char *buf)
@@ -190,7 +189,7 @@ static const struct sysfs_ops dlm_attr_ops = {
 };
 
 static struct kobj_type dlm_ktype = {
-	.default_groups = dlm_groups,
+	.default_attrs = dlm_attrs,
 	.sysfs_ops     = &dlm_attr_ops,
 	.release       = lockspace_kobj_release,
 };
