81be94e291e9974417fb97198d47f0c328a0d6a3
atndko <z1281552865@gmail.com>
2020-10-06 21:08:38 +0800

    Revert "Revert "qcacld-3.0: Enable SAE and GCMP by default""
    This reverts commit 153e20e3f87493be528dd9613be028abdd51d867.
    
    Signed-off-by: atndko <z1281552865@gmail.com>

--- drivers/staging/qcacld-3.0/core/hdd/inc/wlan_hdd_cfg.h
+++ drivers/staging/qcacld-3.0/core/hdd/inc/wlan_hdd_cfg.h
@@ -13868,7 +13868,7 @@ enum hdd_external_acs_policy {
  */
 
 #define CFG_IS_SAE_ENABLED_NAME    "sae_enabled"
-#define CFG_IS_SAE_ENABLED_DEFAULT (0)
+#define CFG_IS_SAE_ENABLED_DEFAULT (1)
 #define CFG_IS_SAE_ENABLED_MIN     (0)
 #define CFG_IS_SAE_ENABLED_MAX     (1)
 
@@ -15336,7 +15336,7 @@ enum hdd_external_acs_policy {
 #define CFG_ENABLE_GCMP_NAME    "gcmp_enabled"
 #define CFG_ENABLE_GCMP_MIN     (0)
 #define CFG_ENABLE_GCMP_MAX     (1)
-#define CFG_ENABLE_GCMP_DEFAULT (0)
+#define CFG_ENABLE_GCMP_DEFAULT (1)
 
 /*
  * <ini>
