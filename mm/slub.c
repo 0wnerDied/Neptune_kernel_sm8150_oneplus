23c82cc4d11b3602b0b10bf3e450b66a15f0f36e
Abel Wu <wuyun.wu@huawei.com>
2020-10-13 16:48:43 -0700

    mm/slub: fix missing ALLOC_SLOWPATH stat when bulk alloc
    The ALLOC_SLOWPATH statistics is missing in bulk allocation now.  Fix it
    by doing statistics in alloc slow path.
    
    Signed-off-by: Abel Wu <wuyun.wu@huawei.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: Pekka Enberg <penberg@kernel.org>
    Acked-by: David Rientjes <rientjes@google.com>
    Cc: Christoph Lameter <cl@linux.com>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Cc: Hewenliang <hewenliang4@huawei.com>
    Cc: Hu Shiyuan <hushiyuan@huawei.com>
    Link: http://lkml.kernel.org/r/20200811022427.1363-1-wuyun.wu@huawei.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: atndko <z1281552865@gmail.com>

--- mm/slub.c
+++ mm/slub.c
@@ -2623,6 +2623,8 @@ static void *___slab_alloc(struct kmem_cache *s, gfp_t gfpflags, int node,
 	void *freelist;
 	struct page *page;
 
+	stat(s, ALLOC_SLOWPATH);
+
 	page = c->page;
 	if (!page) {
 		/*
@@ -2811,7 +2813,6 @@ redo:
 	page = c->page;
 	if (unlikely(!object || !node_match(page, node))) {
 		object = __slab_alloc(s, gfpflags, node, addr, c);
-		stat(s, ALLOC_SLOWPATH);
 	} else {
 		void *next_object = get_freepointer_safe(s, object);
 
