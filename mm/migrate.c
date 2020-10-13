1910fdeb81ed35c963c9fd46df9817f91b8f090f
Ralph Campbell <rcampbell@nvidia.com>
2020-10-13 16:58:39 -0700

    mm/migrate: remove cpages-- in migrate_vma_finalize()
    The variable struct migrate_vma->cpages is only used in
    migrate_vma_setup().  There is no need to decrement it in
    migrate_vma_finalize() since it is never checked.
    
    Signed-off-by: Ralph Campbell <rcampbell@nvidia.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Jason Gunthorpe <jgg@nvidia.com>
    Cc: Jerome Glisse <jglisse@redhat.com>
    Cc: John Hubbard <jhubbard@nvidia.com>
    Cc: Christoph Hellwig <hch@lst.de>
    Link: http://lkml.kernel.org/r/20200827190735.12752-1-rcampbell@nvidia.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: atndko <z1281552865@gmail.com>

--- mm/migrate.c
+++ mm/migrate.c
@@ -2859,7 +2859,6 @@ static void migrate_vma_finalize(struct migrate_vma *migrate)
 
 		remove_migration_ptes(page, newpage, false);
 		unlock_page(page);
-		migrate->cpages--;
 
 		if (is_zone_device_page(page))
 			put_page(page);
