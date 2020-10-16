4ecab32746a3d6f09dd055c9ded62e3e9ff56ff0
atndko <z1281552865@gmail.com>
2020-10-16 20:55:18 +0800

    bui_sh: Update ZIPNAME
    Signed-off-by: atndko <z1281552865@gmail.com>

--- build_master.sh
+++ build_master.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+KNAME="NeptuneKernel"
 MIN_HEAD=$(git rev-parse HEAD)
 VERSION="$(cat version)-$(date +%Y%m%d)-$(echo ${MIN_HEAD:0:8})"
+ZIPNAME="${KNAME}-$(cat version)-$(echo ${MIN_HEAD:0:8})"
 
-export LOCALVERSION="-NeptuneKernel-$(echo "${VERSION}")"
+export LOCALVERSION="-${KNAME}-$(echo "${VERSION}")"
 
 if [[ "${1}" != "skip" ]] ; then
 	./build_clean.sh
@@ -15,16 +17,16 @@ if [ -e arch/arm64/boot/Image.gz ] ; then
 	echo
 	echo "Building Kernel Package"
 	echo
-	rm NeptuneKernel-$VERSION.zip 2>/dev/null
+	rm $ZIPNAME.zip 2>/dev/null
 	rm -rf kernelzip 2>/dev/null
 	mkdir kernelzip
 	cp -rp flasher/* kernelzip/
 	find arch/arm64/boot/dts -name '*.dtb' -exec cat {} + > kernelzip/dtb
 	cd kernelzip/
-	7z a -mx9 NeptuneKernel-$VERSION-tmp.zip *
-	7z a -mx0 NeptuneKernel-$VERSION-tmp.zip ../arch/arm64/boot/Image.gz
-	zipalign -v 4 NeptuneKernel-$VERSION-tmp.zip ../NeptuneKernel-$VERSION.zip
-	rm NeptuneKernel-$VERSION-tmp.zip
+	7z a -mx9 $ZIPNAME-tmp.zip *
+	7z a -mx0 $ZIPNAME-tmp.zip ../arch/arm64/boot/Image.gz
+	zipalign -v 4 $ZIPNAME-tmp.zip ../$ZIPNAME.zip
+	rm $ZIPNAME-tmp.zip
 	cd ..
-	ls -al NeptuneKernel-$VERSION.zip
+	ls -al $ZIPNAME.zip
 fi
