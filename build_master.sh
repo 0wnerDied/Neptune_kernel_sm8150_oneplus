8df85cba553b4c2074293480553a921b509f1d11
atndko <z1281552865@gmail.com>
2020-10-17 21:10:08 +0800

    bui_sh: Touch props for flasher
    Signed-off-by: atndko <z1281552865@gmail.com>

--- build_master.sh
+++ build_master.sh
@@ -20,6 +20,28 @@ if [ -e arch/arm64/boot/Image.gz ] ; then
 	rm $ZIPNAME.zip 2>/dev/null
 	rm -rf kernelzip 2>/dev/null
 	mkdir kernelzip
+	echo "kernel.string=Neptune Kernel $(cat version) by Ciel_U.T
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=OnePlus7
+device.name2=guacamoleb
+device.name3=OnePlus7Pro
+device.name4=guacamole
+device.name5=OnePlus7ProTMO
+device.name6=guacamolet
+device.name7=OnePlus7T
+device.name8=hotdogb
+device.name9=OnePlus7TPro
+device.name10=hotdog
+device.name11=OnePlus7TProNR
+device.name12=hotdogg
+supported.versions=10 - 11
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=1;
+ramdisk_compression=auto;" > kernelzip/props
 	cp -rp flasher/* kernelzip/
 	find arch/arm64/boot/dts -name '*.dtb' -exec cat {} + > kernelzip/dtb
 	cd kernelzip/
