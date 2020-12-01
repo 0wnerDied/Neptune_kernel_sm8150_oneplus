78ce21dade2a4ded8d3c07a1af594c18ecc69a4d
atndko <z1281552865@gmail.com>
2020-12-01 20:34:39 +0800

    cpuidle: Shut down spam
    Signed-off-by: atndko <z1281552865@gmail.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -110,7 +110,7 @@ module_param_named(sleep_disabled, sleep_disabled, bool, 0664);
 void msm_cpuidle_set_sleep_disable(bool disable)
 {
 	sleep_disabled = disable;
-	pr_info("%s:sleep_disabled=%d\n", __func__, disable);
+	pr_debug("%s:sleep_disabled=%d\n", __func__, disable);
 }
 
 /**
