b3c210ea0171370313b58164325b8c76fb5467e5
Sultan Alsawaf <sultan@kerneltoast.com>
2020-11-30 19:13:14 -0800

    cpuidle: lpm-levels: Fix panics when CONFIG_MSM_IDLE_STATS=n
    The cluster->stats is garbage when CONFIG_MSM_IDLE_STATS=n, in which
    case it shouldn't be dereferenced.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: atndko <z1281552865@gmail.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -1243,7 +1243,7 @@ static void cluster_prepare(struct lpm_cluster *cluster,
 	if (cluster_configure(cluster, i, from_idle, predicted))
 		goto failed;
 
-	if (!IS_ERR_OR_NULL(cluster->stats))
+	if ((!IS_ERR_OR_NULL(cluster->stats)) && (IS_ENABLED(CONFIG_MSM_IDLE_STATS)))
 		cluster->stats->sleep_time = start_time;
 	cluster_prepare(cluster->parent, &cluster->num_children_in_sync, i,
 			from_idle, start_time);
@@ -1252,7 +1252,7 @@ static void cluster_prepare(struct lpm_cluster *cluster,
 	return;
 failed:
 	spin_unlock(&cluster->sync_lock);
-	if (!IS_ERR_OR_NULL(cluster->stats))
+	if ((!IS_ERR_OR_NULL(cluster->stats)) && (IS_ENABLED(CONFIG_MSM_IDLE_STATS)))
 		cluster->stats->sleep_time = 0;
 }
 
@@ -1292,7 +1292,8 @@ static void cluster_unprepare(struct lpm_cluster *cluster,
 	if (!first_cpu || cluster->last_level == cluster->default_level)
 		goto unlock_return;
 
-	if (!IS_ERR_OR_NULL(cluster->stats) && cluster->stats->sleep_time)
+	if (!IS_ERR_OR_NULL(cluster->stats) && IS_ENABLED(CONFIG_MSM_IDLE_STATS)
+		&& cluster->stats->sleep_time)
 		cluster->stats->sleep_time = end_time -
 			cluster->stats->sleep_time;
 	lpm_stats_cluster_exit(cluster->stats, cluster->last_level, success);
@@ -1734,7 +1735,7 @@ static void register_cluster_lpm_stats(struct lpm_cluster *cl,
 
 	cl->stats = lpm_stats_config_level(cl->cluster_name, level_name,
 			cl->nlevels, parent ? parent->stats : NULL, NULL);
-	if (IS_ERR_OR_NULL(cl->stats))
+	if (IS_ERR_OR_NULL(cl->stats) && IS_ENABLED(CONFIG_MSM_IDLE_STATS))
 		pr_info("Cluster (%s) stats not registered\n",
 			cl->cluster_name);
 
