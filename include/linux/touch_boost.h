a596adc1fb819339c04030f2f22babe46c0b4ef2
atndko <z1281552865@gmail.com>
2020-12-01 19:08:43 +0800

    sched: touch_boost: Include a touch-based boosting framework
    This is a simple boost driver that boosts some options that I
    think can improve fluency. All events are speed up when the
    device on touchscreen and touchpad input.
    
    While these options may increase some power consumption, there're
    user-exposed knobs to configure it. Also all necessary configuration
    could be done via the supplied Kconfig options.
    
    Signed-off-by: atndko <z1281552865@gmail.com>

---
+++ include/linux/touch_boost.h
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2020 atndko <z1281552865@gmail.com>
+ */
+
+#include <linux/input.h>
+#include <linux/kthread.h>
+#include <linux/moduleparam.h>
+#include <linux/msm_drm_notify.h>
+#include <linux/slab.h>
+#include <uapi/linux/sched/types.h>
+
+#ifndef _TOUCH_BOOST_H_
+#define _TOUCH_BOOST_H_
+
+enum {
+	SCREEN_ON
+};
+
+struct boost_drv {
+	struct notifier_block msm_drm_notif;
+	wait_queue_head_t boost_waitq;
+	unsigned long state;
+};
+
+#endif /* _TOUCH_BOOST_H_ */
