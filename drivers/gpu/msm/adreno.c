e95dff0f10ff521a561e085d0abca34ba8057dcc
atndko <z1281552865@gmail.com>
2020-12-14 22:51:42 +0800

    msm: adreno: Properly set GPU idle timeout to 58 ms
    Signed-off-by: atndko <z1281552865@gmail.com>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -1005,7 +1005,7 @@ static int adreno_of_get_power(struct adreno_device *adreno_dev,
 		device->pwrctrl.pm_qos_wakeup_latency = 100;
 
 	if (of_property_read_u32(node, "qcom,idle-timeout", &timeout))
-		timeout = 64;
+		timeout = 58;
 
 	device->pwrctrl.interval_timeout = msecs_to_jiffies(timeout);
 
