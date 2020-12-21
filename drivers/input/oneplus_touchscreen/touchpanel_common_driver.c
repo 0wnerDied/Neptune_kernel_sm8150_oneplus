f95b4f4fac9fe1cb13db57b6c9257d8922150abf
atndko <z1281552865@gmail.com>
2020-12-21 22:53:03 +0800

    Revert "Reapply 20c2536 ("drivers: op_ts: Mark IRQ as performance critical")"
    This reverts commit 855e2f78d5c12df5ee785cb61e870f151d76fb8b.
    
    Signed-off-by: atndko <z1281552865@gmail.com>

--- drivers/input/oneplus_touchscreen/touchpanel_common_driver.c
+++ drivers/input/oneplus_touchscreen/touchpanel_common_driver.c
@@ -2894,7 +2894,7 @@ static int tp_register_irq_func(struct touchpanel_data *ts)
 			  ts->hw_res.irq_gpio, ts->irq);
 		ret =
 		    request_threaded_irq(ts->irq, NULL, tp_irq_thread_fn,
-					 ts->irq_flags | IRQF_ONESHOT | IRQF_PERF_CRITICAL,
+					 ts->irq_flags | IRQF_ONESHOT,
 					 TPD_DEVICE, ts);
 		if (ret < 0) {
 			TPD_INFO("%s request_threaded_irq ret is %d\n",
