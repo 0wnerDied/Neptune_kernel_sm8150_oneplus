ae9352b5c0468dceda7e80e440e26c1d381e4439
atndko <z1281552865@gmail.com>
2020-12-30 00:07:30 +0800

    Revert "kernel: Add tri-cluster API to affine IRQs and kthreads to fast CPUs"
    This reverts commit fc6234f9af189259e883c931d67a1403c3d27861.
    
    Signed-off-by: atndko <z1281552865@gmail.com>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -1035,18 +1035,7 @@ static int cpu_down_maps_locked(unsigned int cpu, enum cpuhp_state target)
 
 static int do_cpu_down(unsigned int cpu, enum cpuhp_state target)
 {
-	struct cpumask newmask;
 	int err;
-
-	preempt_disable();
-	cpumask_andnot(&newmask, cpu_online_mask, cpumask_of(cpu));
-	preempt_enable();
-
-	/* One big, LITTLE, and prime CPU must remain online */
-	if (!cpumask_intersects(&newmask, cpu_lp_mask) ||
-	    !cpumask_intersects(&newmask, cpu_perf_mask) ||
-	    !cpumask_intersects(&newmask, cpu_prime_mask))
-		return -EINVAL;
 
 	/*
 	 * When cpusets are enabled, the rebuilding of the scheduling
