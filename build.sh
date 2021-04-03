6c0e1b502aff6da6e7d9a403c791f8b466208e6d
atndko <z1281552865@gmail.com>
2021-04-03 23:18:48 +0800

    neptune: Import build scripts
    Signed-off-by: atndko <z1281552865@gmail.com>

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# HOME path
+export HOME=/home/atndko
+
+# Compiler environment
+export CLANG_PATH=$HOME/clang/bin
+export PATH="$CLANG_PATH:$PATH"
+export CROSS_COMPILE=aarch64-linux-gnu-
+export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+export KBUILD_BUILD_USER=Vwool0xE9
+export KBUILD_BUILD_HOST=Atndko
+
+echo
+echo "Setting defconfig"
+echo
+
+make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip neptune_defconfig
+
+echo
+echo "Compiling kernel"
+echo
+
+make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip -j$(nproc --all) || exit 1
---
+++ pack.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Kernel version configuration
+KNAME="NeptuneKernel"
+MIN_HEAD=$(git rev-parse HEAD)
+VERSION="$(cat version)-$(date +%m.%d.%y)-$(echo ${MIN_HEAD:0:8})"
+ZIPNAME="${KNAME}-$(cat version)-$(echo ${MIN_HEAD:0:8})"
+
+export LOCALVERSION="-${KNAME}-$(echo "${VERSION}")"
+
+# Never dirty compile
+if [[ "${1}" != "skip" ]] ; then
+	./clean.sh
+fi
+
+# Let's build
+START=$(date +"%s")
+./build.sh || exit 1
+
+# Kernel Output
+if [ -e arch/arm64/boot/Image.gz ] ; then
+	echo
+	echo "Building Kernel Package"
+	echo
+	rm $ZIPNAME.zip 2>/dev/null
+	rm -rf kernelzip 2>/dev/null
+	# Import Anykernel3 folder
+	mkdir kernelzip
+	cp -rp scripts/AnyKernel3/* kernelzip/
+	find arch/arm64/boot/dts -name '*.dtb' -exec cat {} + > kernelzip/dtb
+	cd kernelzip/
+	7z a -mx9 $ZIPNAME-tmp.zip *
+	7z a -mx0 $ZIPNAME-tmp.zip ../arch/arm64/boot/Image.gz
+	zipalign -v 4 $ZIPNAME-tmp.zip ../$ZIPNAME.zip
+	rm $ZIPNAME-tmp.zip
+	cd ..
+	ls -al $ZIPNAME.zip
+fi
+
+# Show compilation time
+END=$(date +"%s")
+DIFF=$((END - START))
+echo -e "Kernel compiled successfully in $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
