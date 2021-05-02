ccdb79e00a7a72a91536e725f00741b1cececd4e
atndko <z1281552865@gmail.com>
2021-05-02 19:40:13 +0800

    Revert "sched: fix issue of cpu freq running at max always"
    * Well, since CAF patch 6a6e3443e32d0cc53df136f84fde2cd9df00fc80 ("sched: use rq_clock if WALT is not enabled"), this
      issue has a better fix now, revert it.
    
    This reverts commit 9b616a96f980c80707532749681932840b89863b.
    
    Signed-off-by: atndko <z1281552865@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1945,7 +1945,10 @@ static inline int hrtick_enabled(struct rq *rq)
 #ifdef CONFIG_SCHED_WALT
 u64 sched_ktime_clock(void);
 #else
-#define sched_ktime_clock ktime_get_ns
+static inline u64 sched_ktime_clock(void)
+{
+	return sched_clock();
+}
 #endif
 
 #ifdef CONFIG_SMP
