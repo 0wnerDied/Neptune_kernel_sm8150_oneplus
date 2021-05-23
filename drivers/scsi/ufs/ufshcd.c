005bb3d03ff5a87852ac787012a69006ad7c9dac
atndko <z1281552865@gmail.com>
2021-05-23 19:17:50 +0800

    Revert "scsi: ufs: disable hibern8_on_idle"
    This reverts commit 7772b6dc510e8d18e58a6e582b05b952a7ad4398.
    
    Signed-off-by: atndko <z1281552865@gmail.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -2991,9 +2991,6 @@ static ssize_t ufshcd_hibern8_on_idle_enable_store(struct device *dev,
 	struct ufs_hba *hba = dev_get_drvdata(dev);
 	u32 value;
 
-	if (!ufshcd_is_hibern8_on_idle_allowed(hba))
-		return count;
-
 	if (kstrtou32(buf, 0, &value))
 		return -EINVAL;
 
@@ -3015,7 +3012,8 @@ static void ufshcd_init_hibern8_on_idle(struct ufs_hba *hba)
 	/* initialize the state variable here */
 	hba->hibern8_on_idle.state = HIBERN8_EXITED;
 
-	if (!ufshcd_is_hibern8_on_idle_allowed(hba))
+	if (!ufshcd_is_hibern8_on_idle_allowed(hba) &&
+	    !ufshcd_is_auto_hibern8_supported(hba))
 		return;
 
 	if (ufshcd_is_auto_hibern8_supported(hba)) {
@@ -3059,7 +3057,8 @@ static void ufshcd_init_hibern8_on_idle(struct ufs_hba *hba)
 
 static void ufshcd_exit_hibern8_on_idle(struct ufs_hba *hba)
 {
-	if (!ufshcd_is_hibern8_on_idle_allowed(hba))
+	if (!ufshcd_is_hibern8_on_idle_allowed(hba) &&
+	    !ufshcd_is_auto_hibern8_supported(hba))
 		return;
 	device_remove_file(hba->dev, &hba->hibern8_on_idle.delay_attr);
 	device_remove_file(hba->dev, &hba->hibern8_on_idle.enable_attr);
