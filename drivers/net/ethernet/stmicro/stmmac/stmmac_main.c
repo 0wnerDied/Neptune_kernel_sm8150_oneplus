2e713aba02e243fd2fe10ca7e09c81a345e2e8a3
Sagar Cheluvegowda <scheluve@codeaurora.org>
2021-06-08 22:48:31 -0700

    net:stmmac:Update AHB clock for MDIO read/write operations
    To avoid MDIO clock from drifting below 1.4Mhz which is not
    allowed as per IEEE spec, this change makes sures that
    everytime when emac wants to do a phy read/write operations,
    MDIO always makes a nominal vote to the AHB bus for clock
    and switches back to the actual vote once the operation is done.
    
    Change-Id: I75e8773ec553cee64d7549a936d7dccf6829ac4e
    Signed-off-by: Sagar Cheluvegowda <scheluve@codeaurora.org>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -2826,6 +2826,9 @@ static int stmmac_open(struct net_device *dev)
 	struct stmmac_priv *priv = netdev_priv(dev);
 	int ret;
 
+	if (priv->plat->update_ahb_clk_cfg)
+		priv->plat->update_ahb_clk_cfg(priv, 1, 1);
+
 	if (!priv->plat->mac2mac_en &&
 	    (priv->hw->pcs != STMMAC_PCS_RGMII &&
 	     priv->hw->pcs != STMMAC_PCS_TBI &&
@@ -2924,6 +2927,9 @@ static int stmmac_open(struct net_device *dev)
 		netif_carrier_on(dev);
 	}
 
+	if (priv->plat->update_ahb_clk_cfg)
+		priv->plat->update_ahb_clk_cfg(priv, 0, 0);
+
 	return 0;
 
 lpiirq_error:
@@ -2942,6 +2948,9 @@ init_error:
 dma_desc_error:
 	if (dev->phydev)
 		phy_disconnect(dev->phydev);
+
+	if (priv->plat->update_ahb_clk_cfg)
+		priv->plat->update_ahb_clk_cfg(priv, 0, 0);
 
 	return ret;
 }
