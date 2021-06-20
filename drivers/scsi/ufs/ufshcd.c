d26d841f5b953e921d865bd5492a64eba29db7cf
atndko <z1281552865@gmail.com>
2021-06-20 14:51:06 +0800

    scsi: ufs: Fix format specifier warning
    Signed-off-by: atndko <z1281552865@gmail.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -3853,7 +3853,7 @@ static int ufshcd_queuecommand(struct Scsi_Host *host, struct scsi_cmnd *cmd)
 	if (err) {
 		if (err != -EAGAIN)
 			dev_err(hba->dev,
-				"%s: failed to compose upiu %d cmd:0x%08x lun:%d\n",
+				"%s: failed to compose upiu %d cmd:0x%pK lun:%d\n",
 				__func__, err, cmd, lrbp->lun);
 
 		lrbp->cmd = NULL;
