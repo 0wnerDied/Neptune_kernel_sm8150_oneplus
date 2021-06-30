7f4948424d80987d994f5258854428fcec42d8b4
atndko <z1281552865@gmail.com>
2021-06-30 20:31:33 +0800

    Revert "treewide: Remove dirty codes from OnePlus"
    This reverts commit 91be6a66e6ef74917ebe5d299f8ad8b1f7f878d9.
    
    * This is a partly revert of commit "treewide: Remove dirty codes from OnePlus".
    
    Fixes: 91be6a66e6ef74917ebe5d299f8ad8b1f7f878d9 ("treewide: Remove dirty codes from OnePlus")
    Signed-off-by: atndko <z1281552865@gmail.com>

--- drivers/rtc/qpnp-rtc.c
+++ drivers/rtc/qpnp-rtc.c
@@ -87,6 +87,14 @@ static int qpnp_write_wrapper(struct qpnp_rtc *rtc_dd, u8 *rtc_val,
 			u16 base, int count)
 {
 	int rc;
+	static u8 rtc_val_pre;
+
+	if (base == (rtc_dd->alarm_base + REG_OFFSET_ALARM_CTRL1)) {
+		if (*rtc_val != rtc_val_pre)
+			dev_dbg(rtc_dd->rtc_dev,
+				"write ALARM_CTRL1=0x%x\n", *rtc_val);
+		rtc_val_pre = *rtc_val;
+	}
 
 	rc = regmap_bulk_write(rtc_dd->regmap, base, rtc_val, count);
 	if (rc) {
@@ -285,6 +293,7 @@ qpnp_rtc_set_alarm(struct device *dev, struct rtc_wkalrm *alarm)
 {
 	int rc;
 	u8 value[4], ctrl_reg;
+	static u8 pre_value_0, pre_value_1, pre_value_2, pre_value_3;
 	unsigned long secs, secs_rtc, irq_flags;
 	struct qpnp_rtc *rtc_dd = dev_get_drvdata(dev);
 	struct rtc_time rtc_tm;
@@ -311,6 +320,16 @@ qpnp_rtc_set_alarm(struct device *dev, struct rtc_wkalrm *alarm)
 	value[1] = (secs >> 8) & 0xFF;
 	value[2] = (secs >> 16) & 0xFF;
 	value[3] = (secs >> 24) & 0xFF;
+
+	if (value[0] != pre_value_0 || value[1] != pre_value_1
+				|| value[2] != pre_value_2 || value[3] != pre_value_3) {
+		dev_dbg(dev, "val[0] = 0x%x, val[1] = 0x%x, val[2] = 0x%x, val[3] = 0x%x\n",
+					value[0], value[1], value[2], value[3]);
+	}
+	pre_value_0 = value[0];
+	pre_value_1 = value[1];
+	pre_value_2 = value[2];
+	pre_value_3 = value[3];
 
 	spin_lock_irqsave(&rtc_dd->alarm_ctrl_lock, irq_flags);
 
@@ -652,11 +671,17 @@ static void qpnp_rtc_shutdown(struct platform_device *pdev)
 	unsigned long irq_flags;
 	struct qpnp_rtc *rtc_dd;
 	bool rtc_alarm_powerup;
+	struct rtc_wkalrm alarm;
 
 	if (!pdev) {
 		pr_err("qpnp-rtc: spmi device not found\n");
 		return;
 	}
+	qpnp_rtc_read_alarm(&pdev->dev, &alarm);
+	dev_dbg(&pdev->dev, "Alarm set for - h:r:s=%d:%d:%d, d/m/y=%d/%d/%d\n",
+			alarm.time.tm_hour, alarm.time.tm_min,
+			alarm.time.tm_sec, alarm.time.tm_mday,
+			alarm.time.tm_mon, alarm.time.tm_year);
 	rtc_dd = dev_get_drvdata(&pdev->dev);
 	if (!rtc_dd) {
 		pr_err("qpnp-rtc: rtc driver data not found\n");
--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -36,9 +36,11 @@
 #include "power.h"
 #include <soc/qcom/boot_stats.h>
 
+#include <linux/gpio.h>
 #include <linux/soc/qcom/smem_state.h>
 extern struct qcom_smem_state *qstate;
-#define AWAKE_BIT BIT(12) /* 12th bit */
+#define PROC_AWAKE_ID 12 /* 12th bit */
+#define AWAKE_BIT BIT(PROC_AWAKE_ID)
 
 const char * const pm_labels[] = {
 	[PM_SUSPEND_TO_IDLE] = "freeze",
@@ -67,6 +69,7 @@ static DECLARE_WAIT_QUEUE_HEAD(s2idle_wait_head);
 
 enum s2idle_states __read_mostly s2idle_state;
 static DEFINE_RAW_SPINLOCK(s2idle_lock);
+extern int check_touchirq_triggerd(void);
 
 void s2idle_set_ops(const struct platform_s2idle_ops *ops)
 {
@@ -445,7 +448,11 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 
 	arch_suspend_disable_irqs();
 	BUG_ON(!irqs_disabled());
-
+	if (check_touchirq_triggerd()) {
+		pr_debug("[TP]touchpanel irq status is low\n");
+		error = -EBUSY;
+		goto Enable_irq;
+	}
 	error = syscore_suspend();
 	if (!error) {
 		*wakeup = pm_wakeup_pending();
@@ -461,7 +468,7 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 		}
 		syscore_resume();
 	}
-
+Enable_irq:
 	arch_suspend_enable_irqs();
 	BUG_ON(irqs_disabled());
 
@@ -637,14 +644,14 @@ int pm_suspend(suspend_state_t state)
 		return -EINVAL;
 
 	qcom_smem_state_update_bits(qstate, AWAKE_BIT, 0);
-	pr_err("%s: PM_SUSPEND_PREPARE smp2p_change_state", __func__); 
+	pr_info("%s: PM_SUSPEND_PREPARE smp2p_change_state", __func__); 
 
 	pm_suspend_marker("entry");
 	pr_info("suspend entry (%s)\n", mem_sleep_labels[state]);
 	error = enter_state(state);
 
 	qcom_smem_state_update_bits(qstate, AWAKE_BIT, AWAKE_BIT);
-	pr_err("%s: PM_POST_SUSPEND smp2p_change_state", __func__);
+	pr_info("%s: PM_POST_SUSPEND smp2p_change_state", __func__);
 
 	if (error) {
 		suspend_stats.fail++;
