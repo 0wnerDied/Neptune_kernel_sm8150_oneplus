a6c7a4bf766ef6f3c5a367340d2fea9246834f2b
atndko <z1281552865@gmail.com>
2021-07-01 14:29:24 +0800

    Revert "fs: Improve eventpoll logging to stop indicting timerfd"
    This reverts commit 6eca2f1f3da72b20f5bca036ef49bc87f6e0894c.
    
    Signed-off-by: atndko <z1281552865@gmail.com>

--- fs/eventpoll.c
+++ fs/eventpoll.c
@@ -1392,23 +1392,15 @@ static int ep_create_wakeup_source(struct epitem *epi)
 {
 	struct name_snapshot n;
 	struct wakeup_source *ws;
-	char task_comm_buf[TASK_COMM_LEN];
-	char buf[64];
-
-	get_task_comm(task_comm_buf, current);
 
 	if (!epi->ep->ws) {
-		snprintf(buf, sizeof(buf), "epoll_%.*s_epollfd",
-			 (int)sizeof(task_comm_buf), task_comm_buf);
-		epi->ep->ws = wakeup_source_register(NULL, buf);
+		epi->ep->ws = wakeup_source_register(NULL, "eventpoll");
 		if (!epi->ep->ws)
 			return -ENOMEM;
 	}
 
 	take_dentry_name_snapshot(&n, epi->ffd.file->f_path.dentry);
-	snprintf(buf, sizeof(buf), "epoll_%.*s_file:%s",
-		 (int)sizeof(task_comm_buf), task_comm_buf, n.name);
-	ws = wakeup_source_register(NULL, buf);
+	ws = wakeup_source_register(NULL, n.name);
 	release_dentry_name_snapshot(&n);
 
 	if (!ws)
