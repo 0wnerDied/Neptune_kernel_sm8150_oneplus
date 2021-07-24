4530a1c0bd74e7a9665635199d1e4981dbd9f16d
Nagarjuna Chaganti <nchagant@codeaurora.org>
2021-07-24 11:28:26 +0530

    net: stmmac: Handling SSR only if init is complete
    For targets with IPA offload enabled, SSR needs to be handled
    only and only if ETH IPA offload is complete. Otherwise its sane
    to ignore SSR events.
    
    Change-Id: I439cddf50d6e2ec2aa0b80c4361ece20dd02526d
    Signed-off-by: Nagarjuna Chaganti <nchagant@codeaurora.org>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-qcom-ipa.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-qcom-ipa.c
@@ -3272,11 +3272,23 @@ void ethqos_ipa_offload_event_handler(void *data,
 
 	switch (ev) {
 	case EV_IPA_SSR_UP:
+		if (!eth_ipa_ctx.emac_dev_ready ||
+		    !eth_ipa_ctx.ipa_uc_ready ||
+		    !eth_ipa_ctx.ipa_offload_susp[IPA_QUEUE_CV2X] ||
+		    !eth_ipa_ctx.ipa_offload_conn)
+			break;
+
 		ethqos_ipa_offload_resume(eth_ipa_ctx.ethqos, IPA_QUEUE_CV2X,
 					  false);
 		eth_ipa_ctx.ipa_offload_susp[IPA_QUEUE_CV2X] = false;
 		break;
 	case EV_IPA_SSR_DOWN:
+		if (!eth_ipa_ctx.emac_dev_ready ||
+		    !eth_ipa_ctx.ipa_uc_ready ||
+		    eth_ipa_ctx.ipa_offload_susp[IPA_QUEUE_CV2X] ||
+		    !eth_ipa_ctx.ipa_offload_conn)
+			break;
+
 		ethqos_ipa_offload_suspend(eth_ipa_ctx.ethqos, IPA_QUEUE_CV2X,
 					   false);
 		eth_ipa_ctx.ipa_offload_susp[IPA_QUEUE_CV2X] = true;
