4292a514c989c91db2e216ff89f7416fc68cf78d
atndko <z1281552865@gmail.com>
2021-08-12 21:22:56 +0800

    neptune: Use AOSP clang 13.0.1
    Signed-off-by: atndko <z1281552865@gmail.com>

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@
 export HOME=/home/atndko
 
 # Compiler environment
-export CLANG_PATH=$HOME/clang/bin
+export CLANG_PATH=$HOME/linux-x86/clang-r428724/bin
 export PATH="$CLANG_PATH:$PATH"
-export CROSS_COMPILE=aarch64-linux-gnu-
-export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+export CROSS_COMPILE=$HOME/clang/bin/aarch64-linux-gnu-
+export CROSS_COMPILE_ARM32=$HOME/clang/bin/arm-linux-gnueabi-
 export KBUILD_BUILD_USER=Vwool0xE9
 export KBUILD_BUILD_HOST=Atndko
 
