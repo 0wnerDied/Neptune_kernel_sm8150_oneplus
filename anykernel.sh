ea64f3dd0c957665368d422692501ee266622a13
atndko <z1281552865@gmail.com>
2021-11-13 17:38:55 +0800

    AK3: Add Android S support
    Signed-off-by: atndko <z1281552865@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -22,7 +22,7 @@ device.name9=OnePlus7TPro
 device.name10=hotdog
 device.name11=OnePlus7TProNR
 device.name12=hotdogg
-supported.versions=11
+supported.versions=11 - 12
 '; } # end properties
 
 # shell variables
