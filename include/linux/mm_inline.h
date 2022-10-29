e47d5e308bb80b9e1db69162d094856a2d61e67f
Juhyung Park <qkrwngud825@gmail.com>
2022-10-29 15:19:54 +0900

    mm: fix compilation on arm64 after mglru v11 merge
    arm64 doesn't provide try_cmpxchg(), backport generic's arch_try_cmpxchg().
    
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: 0wnerDied <z1281552865@gmail.com>

--- include/linux/mm_inline.h
+++ include/linux/mm_inline.h
@@ -5,6 +5,17 @@
 #include <linux/huge_mm.h>
 #include <linux/swap.h>
 
+#ifndef try_cmpxchg
+#define try_cmpxchg(_ptr, _oldp, _new) \
+({ \
+	typeof(*(_ptr)) *___op = (_oldp), ___o = *___op, ___r; \
+	___r = cmpxchg((_ptr), ___o, (_new)); \
+	if (unlikely(___r != ___o)) \
+		*___op = ___r; \
+	likely(___r == ___o); \
+})
+#endif /* try_cmpxchg */
+
 /**
  * page_is_file_cache - should the page be on a file LRU or anon LRU?
  * @page: the page to test
